cc7a992133f45afb3c6c7e0301ba9b82e4b546bd
Clinterpottrmus <Clinterpottrmus@gmail.com>
2021-04-24 09:53:35 -0800

    Choose character

---
+++ cmake-build-debug/CMakeFiles/CS201_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS201_Project.dir/main.cpp.obj"
+  "CS201_Project.exe"
+  "CS201_Project.exe.manifest"
+  "CS201_Project.pdb"
+  "libCS201_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS201_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*Clint Scholtisek
+ * CS201 end of semester project
+ * 04/19/2021
+ * Jon Genetti
+ * This program is a simple tic-tac-toe game
+ * The computer opponent uses randomization and win conditions
+ */
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <random>
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+
+int main() {
+    //initialize slot vector
+    vector<string> slot(10);
+
+    //Intro
+    cout << "Welcome to Tic Tac Toe" << endl;
+    cout << "This is the board layout" << endl;
+    cout << " 1  |  2  |  3\n"
+            "---------------\n"
+            " 4  |  5  |  6\n"
+            "---------------\n"
+            " 7  |  8  |  9" << endl;
+
+
+    //Choose X or O
+    string player_char;
+    char ai_char;
+    //do loop for valid character input
+    do {
+        cout << "Choose character (x or o)" << endl;
+        //do loop in case empty character input
+        do {
+            getline(cin, player_char);
+            if (player_char.empty()) {
+                cout << "Please enter a valid character (x or o)" << endl;
+            }
+        } while(player_char.empty());
+        //Convert input to lowercase first character
+        player_char = ::tolower(player_char.at(0));
+
+        //assign AI opponent opposite character
+        if (player_char == "x" or player_char == "o") {
+            cout << "You chose " << player_char << endl;
+            if (player_char == "x") {
+                ai_char = 'o';
+            } else{
+                ai_char = 'x';
+            }
+        } else{
+            cout << "Error, please enter x or o" << endl;
+        }
+    } while(player_char != "x" and player_char != "o");
+    return 0;
+}
